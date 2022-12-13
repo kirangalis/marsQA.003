faa311590bf95e800fb15f06d0f5e544c3400a72
kirangalis <111936250+kirangalis@users.noreply.github.com>
2022-12-13 22:18:29 +1300

    Add files via upload

---
+++ LoginPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marsQA_1.pages
+{
+    internal class LoginPage
+    {
+    }
+}
